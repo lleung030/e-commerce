7117b9e661637af5d5da13ebe71aafb50abcdb08
lleung030 <lucasleung2593@gmail.com>
2023-08-17 10:22:57 -0700

    env

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>
-      Admin Dashboard
+    <div className='p-4'>
+      <Button size='default'>Click Me</Button>
     </div>
   )
 }
