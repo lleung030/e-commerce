a768b04182c14d727939e0e447a4825cd80eef66
lleung030 <lucasleung2593@gmail.com>
2023-08-17 10:39:39 -0700

    user sign out

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,14 @@
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+const SetupPage = () => {
   return (
     <div className='p-4'>
-      <Button size='default'>Click Me</Button>
+      This is a protected route!
+      <UserButton afterSignOutUrl="/"/>
     </div>
   )
 }
+
+export default SetupPage
